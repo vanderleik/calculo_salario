b754c085bcc90ed007c1544db76206380d7a1ffb
Vanderlei Kleinschmidt <44402492+vanderleik@users.noreply.github.com>
2022-06-24 08:13:14 -0300

    Add files via upload

---
+++ application/ProgramCalcSalary.java
@@ -0,0 +1,36 @@
+package application;
+
+import entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramCalcSalary {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+        System.out.println("Informe os dados do funcionário: ");
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee: " + employee);
+
+        System.out.println();
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Updated data: " + employee);
+
+        sc.close();
+    }
+}
